41fdb9767879b37517d4bb528be99da6f45ded80
Celestial-glitch <vaishnavidk346@gmail.com>
2025-08-05 16:52:20 +0530

    error

--- next.config.js
+++ next.config.js
@@ -1,15 +1,30 @@
-// import type { NextConfig } from "next";
+// // import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   ignoreDuringBuilds: true,
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// //   ignoreDuringBuilds: true,
+// // };
+
+// // export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
 // };
 
-// export default nextConfig;
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false, //  Prevent face-api.js from crashing build due to 'fs'
+    };
+    return config;
   },
 };
 
